e927a425ff07157f9b3d5161863b694abed35376
Virtus-inc <aliondesh@gmail.com>
2024-05-20 22:21:48 +0300

    Installed Nuxt App

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const { SESSION_SECRET, IS_PRODUCTION } = require('./configs');
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(
   expressSession({
     name: 'rgb-session',
